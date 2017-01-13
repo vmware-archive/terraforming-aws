4b4256c1977b33435c0270f5a0f79b8c8a62e0ea
Dave Walter <dwalter@pivotal.io>
2017-01-13 15:41:19 -0800

    Add subnet cidr outputs.
    [#135574877]
    
    Signed-off-by: Kalai Wei <kawei@pivotal.io>

--- outputs.tf
+++ outputs.tf
@@ -98,6 +98,10 @@ output "director_subnet_availability_zones" {
   value = "${join(",", aws_subnet.director_subnets.*.availability_zone)}"
 }
 
+output "director_subnet_cidrs" {
+  value = "${join(",", aws_subnet.director_subnets.*.cidr_block)}"
+}
+
 output "ert_subnet_ids" {
   value = "${join(",", aws_subnet.ert_subnets.*.id)}"
 }
@@ -106,12 +110,20 @@ output "ert_subnet_availability_zones" {
   value = "${join(",", aws_subnet.ert_subnets.*.availability_zone)}"
 }
 
+output "ert_subnet_cidrs" {
+  value = "${join(",", aws_subnet.ert_subnets.*.cidr_block)}"
+}
+
 output "service_subnet_ids" {
   value = "${join(",", aws_subnet.service_subnets.*.id)}"
 }
 
 output "service_subnet_availability_zones" {
   value = "${join(",", aws_subnet.service_subnets.*.availability_zone)}"
+}
+
+output "service_subnet_cidrs" {
+  value = "${join(",", aws_subnet.service_subnets.*.cidr_block)}"
 }
 
 output "vpc_id" {
