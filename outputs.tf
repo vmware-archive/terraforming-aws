396db5a880aba3324cc365a170896d569a2acbaf
Dave Walter <dwalter@pivotal.io>
2018-05-16 10:43:02 -0700

    Remove empty DNS name server list entries

--- outputs.tf
+++ outputs.tf
@@ -39,7 +39,7 @@ output "optional_ops_manager_dns" {
 }
 
 output "env_dns_zone_name_servers" {
-  value = ["${split(",", local.name_servers)}"]
+  value = ["${compact(split(",", local.name_servers))}"]
 }
 
 output "sys_domain" {
