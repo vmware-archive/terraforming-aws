85f774a490da59681405d3b276451cc1076d8f40
Andrew Crump <acrump@pivotal.io>
2018-05-18 20:02:04 -0700

    WIP
    Signed-off-by: Josh Zarrabi <jzarrabi@pivotal.io>

--- iam.tf
+++ iam.tf
@@ -99,3 +99,24 @@ resource "aws_iam_instance_profile" "pas_bucket_access" {
   name = "${var.env_name}_pas_bucket_access"
   role = "${aws_iam_role.pas_bucket_access.name}"
 }
+
+data "template_file" "pas_backup_bucket_policy" {
+  template = "${file("${path.module}/templates/iam_pas_backup_buckets_policy.json")}"
+
+  vars {
+    buildpacks_backup_bucket_arn = "${aws_s3_bucket.buildpacks_backup_bucket.arn}"
+    droplets_backup_bucket_arn   = "${aws_s3_bucket.droplets_backup_bucket.arn}"
+    packages_backup_bucket_arn   = "${aws_s3_bucket.packages_backup_bucket.arn}"
+    resources_backup_bucket_arn  = "${aws_s3_bucket.resources_backup_bucket.arn}"
+  }
+
+  count = "${var.create_backup_pas_buckets ? 1 : 0}"
+}
+
+resource "aws_iam_role_policy" "pas_backup_bucket_access" {
+  name   = "${var.env_name}_pas_backup_bucket_access"
+  role   = "${aws_iam_role.pas_bucket_access.id}"
+  policy = "${data.template_file.pas_backup_bucket_policy.rendered}"
+
+  count = "${var.create_backup_pas_buckets ? 1 : 0}"
+}
