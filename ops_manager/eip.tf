e01ea4cc342c94f887d8a4c67acffd8fb54135eb
Genevieve Lesperance <glesperance@pivotal.io>
2018-08-29 16:17:57 -0600

    Add tags for address.

--- ops_manager/eip.tf
+++ ops_manager/eip.tf
@@ -2,10 +2,18 @@ resource "aws_eip" "ops_manager" {
   instance = "${aws_instance.ops_manager.id}"
   count    = "${var.private ? 0 : var.count}"
   vpc      = true
+
+  tags = "${merge(var.tags, var.default_tags,
+    map("Name", "${var.env_name}-om-eip")
+  )}"
 }
 
 resource "aws_eip" "optional_ops_manager" {
   instance = "${aws_instance.optional_ops_manager.id}"
   count    = "${var.private ? 0 : var.optional_count}"
   vpc      = true
+
+  tags = "${merge(var.tags, var.default_tags,
+    map("Name", "${var.env_name}-optional-om-eip")
+  )}"
 }
