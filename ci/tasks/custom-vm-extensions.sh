0454b8936e6d58570a4194be10277964d407d360
Chris Dutra <cdutra@pivotal.io>
2018-11-19 14:33:42 -0800

    Fix ssh lb security group vm extension name
    Co-authored-by: Sara Montecino <saramontecino@outlook.com>

--- ci/tasks/custom-vm-extensions.sh
+++ ci/tasks/custom-vm-extensions.sh
@@ -8,7 +8,7 @@ main() {
   if [[ -n "${PAS}" ]]; then
     om -k curl --path /api/v0/staged/vm_extensions/web-lb-security-groups -x PUT -d \
       '{"name": "web-lb-security-groups", "cloud_properties": { "security_groups": ["web_lb_security_group", "vms_security_group"] }}'
-    om -k curl --path /api/v0/staged/vm_extensions/ssh-lb-security-group -x PUT -d \
+    om -k curl --path /api/v0/staged/vm_extensions/ssh-lb-security-groups -x PUT -d \
       '{"name": "ssh-lb-security-groups", "cloud_properties": { "security_groups": ["ssh_lb_security_group", "vms_security_group"] }}'
     om -k curl --path /api/v0/staged/vm_extensions/tcp-lb-security-groups -x PUT -d \
       '{"name": "tcp-lb-security-groups", "cloud_properties": { "security_groups": ["tcp_lb_security_group", "vms_security_group"] }}'
