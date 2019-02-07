9705bdd8bb7da752b7ed793e61c0f39780324df4
fbehrens51 <fbehrens51@gmail.com>
2019-02-07 11:53:12 -0500

    updated references to use data prefix for resources now loaded instead of created.  Also commented out unused (no longer created) resources

--- modules/ops_manager/iam.tf
+++ modules/ops_manager/iam.tf
@@ -1,26 +1,26 @@
-data "template_file" "ops_manager" {
-  template = "${file("${path.module}/templates/iam_policy.json")}"
-
-  vars {
-    iam_instance_profile_arn = "${data.aws_iam_instance_profile.ops_manager.arn}"
-    ops_manager_bucket_arn   = "${aws_s3_bucket.ops_manager_bucket.arn}"
-    iam_ops_manager_role_arn = "${data.aws_iam_role.ops_manager.arn}"
-  }
-}
-
-data "aws_iam_policy_document" "ops_manager" {
-  source_json = "${data.template_file.ops_manager.rendered}"
-
-  statement {
-    sid     = "AllowToCreateInstanceWithCurrentInstanceProfile"
-    effect  = "Allow"
-    actions = ["iam:PassRole"]
-
-    resources = [
-      "${compact(concat(list(data.aws_iam_role.ops_manager.arn), var.additional_iam_roles_arn))}",
-    ]
-  }
-}
+//data "template_file" "ops_manager" {
+//  template = "${file("${path.module}/templates/iam_policy.json")}"
+//
+//  vars {
+//    iam_instance_profile_arn = "${data.aws_iam_instance_profile.ops_manager.arn}"
+//    ops_manager_bucket_arn   = "${aws_s3_bucket.ops_manager_bucket.arn}"
+//    iam_ops_manager_role_arn = "${data.aws_iam_role.ops_manager.arn}"
+//  }
+//}
+//
+//data "aws_iam_policy_document" "ops_manager" {
+//  source_json = "${data.template_file.ops_manager.rendered}"
+//
+//  statement {
+//    sid     = "AllowToCreateInstanceWithCurrentInstanceProfile"
+//    effect  = "Allow"
+//    actions = ["iam:PassRole"]
+//
+//    resources = [
+//      "${compact(concat(list(data.aws_iam_role.ops_manager.arn), var.additional_iam_roles_arn))}",
+//    ]
+//  }
+//}
 
 //resource "aws_iam_policy" "ops_manager_role" {
 //  name   = "${var.env_name}_ops_manager_role"
