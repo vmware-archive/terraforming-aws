726e12aebca834ea247b2fc7c89be1de1be4e120
Ubuntu <fbehrens51@gmail.com>
2019-02-08 15:14:44 +0000

    readability of new Dockerfile

--- packageTF.sh
+++ packageTF.sh
@@ -6,4 +6,5 @@ docker run -it --rm \
        	-v /var/run/docker.sock:/var/run/docker.sock \
        	-v $(pwd):/wks  \
 	-w /wks  \
-	terraform-bundle terraform-bundle package -os=linux -arch=amd64 terraform-bundle.hcl
+	terraform-bundle \
+	terraform-bundle package -os=linux -arch=amd64 terraform-bundle.hcl
