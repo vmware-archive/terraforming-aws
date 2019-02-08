25bb5c94ebcb17e0ff0d3436e00471a9a6168c55
Ubuntu <fbehrens51@gmail.com>
2019-02-08 15:09:53 +0000

    new Package script which mounts and packages to local workspace

---
+++ packageTF.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker build -t terraform-bundle -f DockerfileForMount .
+
+docker run -it --rm \
+       	-v /var/run/docker.sock:/var/run/docker.sock \
+       	-v $(pwd):/wks  \
+	-w /wks  \
+	terraform-bundle terraform-bundle package -os=linux -arch=amd64 terraform-bundle.hcl
