c073cefe4e7a2f48ed5eee0b2dea1065fdf2ee39
fbehrens51 <fbehrens51@gmail.com>
2019-02-12 07:55:18 -0500

    adjusting formatting (tabs/whitespace were off)

--- packageTF.sh
+++ packageTF.sh
@@ -3,8 +3,8 @@
 docker build -t terraform-bundle -f DockerfileForMount .
 
 docker run -it --rm \
-       	-v /var/run/docker.sock:/var/run/docker.sock \
-       	-v $(pwd):/wks  \
-	-w /wks  \
-	terraform-bundle \
-	terraform-bundle package -os=linux -arch=amd64 terraform-bundle.hcl
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v $(pwd):/wks  \
+        -w /wks  \
+        terraform-bundle \
+        terraform-bundle package -os=linux -arch=amd64 terraform-bundle.hcl
