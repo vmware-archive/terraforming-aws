b81e41b97466d9cd18b8dc80e12f9bcd3c8b89f4
Robert Van Voorhees <rcvanvo@gmail.com>
2019-02-15 17:18:38 -0500

    Use Route53 ALIAS entries instead of CNAME (so we can have health checks)

--- modules/pas/dns.tf
+++ modules/pas/dns.tf
@@ -6,38 +6,50 @@ resource "aws_route53_record" "wildcard_sys_dns" {
   count   = "${local.use_route53 ? 1 : 0}"
   zone_id = "${var.zone_id}"
   name    = "*.sys.${var.env_name}.${var.dns_suffix}"
-  type    = "CNAME"
-  ttl     = 300
+  type    = "A"
 
-  records = ["${aws_lb.web.dns_name}"]
+  alias {
+    name                   = "${aws_lb.web.dns_name}"
+    zone_id                = "${aws_lb.web.zone_id}"
+    evaluate_target_health = true
+  }
 }
 
 resource "aws_route53_record" "wildcard_apps_dns" {
   count   = "${local.use_route53 ? 1 : 0}"
   zone_id = "${var.zone_id}"
   name    = "*.apps.${var.env_name}.${var.dns_suffix}"
-  type    = "CNAME"
-  ttl     = 300
+  type    = "A"
 
-  records = ["${aws_lb.web.dns_name}"]
+  alias {
+    name                   = "${aws_lb.web.dns_name}"
+    zone_id                = "${aws_lb.web.zone_id}"
+    evaluate_target_health = true
+  }
 }
 
 resource "aws_route53_record" "ssh" {
   count   = "${local.use_route53 ? 1 : 0}"
   zone_id = "${var.zone_id}"
   name    = "ssh.sys.${var.env_name}.${var.dns_suffix}"
-  type    = "CNAME"
-  ttl     = 300
+  type    = "A"
 
-  records = ["${aws_lb.ssh.dns_name}"]
+  alias {
+    name                   = "${aws_lb.ssh.dns_name}"
+    zone_id                = "${aws_lb.ssh.zone_id}"
+    evaluate_target_health = true
+  }
 }
 
 resource "aws_route53_record" "tcp" {
   count   = "${local.use_route53 ? 1 : 0}"
   zone_id = "${var.zone_id}"
   name    = "tcp.${var.env_name}.${var.dns_suffix}"
-  type    = "CNAME"
-  ttl     = 300
+  type    = "A"
 
-  records = ["${aws_lb.tcp.dns_name}"]
+  alias {
+    name                   = "${aws_lb.tcp.dns_name}"
+    zone_id                = "${aws_lb.tcp.zone_id}"
+    evaluate_target_health = true
+  }
 }
