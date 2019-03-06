0fd495459b967e73a803a6087ce7e8b06b65c751
jgordon <jgordon@pivotal.io>
2019-03-06 15:21:18 -0500

    use only the prefix as key to the map.

--- modules/calculate_subnets/main.tf
+++ modules/calculate_subnets/main.tf
@@ -3,23 +3,25 @@ variable "vpc_cidr" {
 }
 
 locals {
+  cidr_split= "${split("/", var.vpc_cidr)}"
+  cidr_prefix = "${local.cidr_split[1]}"
 
   cidr_breakout_map = {
-    "10.0.0.0/16" = {
+    "16" = {
       "large" = 6
       "small" = 10
       "infra_index" = 64
     }
-    "10.0.0.0/20" = {
+    "20" = {
       "large" = 3
       "small" = 6
       "infra_index" = 32
     }
   }
 
-  newbits_to_large = "${lookup(local.cidr_breakout_map[var.vpc_cidr],"large")}"
-  newbits_to_small = "${lookup(local.cidr_breakout_map[var.vpc_cidr],"small")}"
-  index_for_ifra_subnet = "${lookup(local.cidr_breakout_map[var.vpc_cidr],"infra_index")}"
+  newbits_to_large = "${lookup(local.cidr_breakout_map[local.cidr_prefix],"large")}"
+  newbits_to_small = "${lookup(local.cidr_breakout_map[local.cidr_prefix],"small")}"
+  index_for_ifra_subnet = "${lookup(local.cidr_breakout_map[local.cidr_prefix],"infra_index")}"
 
   rds_cidr = "${cidrsubnet(var.vpc_cidr, local.newbits_to_large, 3)}"
   pas_cidr      = "${cidrsubnet(var.vpc_cidr, local.newbits_to_large, 1)}"
