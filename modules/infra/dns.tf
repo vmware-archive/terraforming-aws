1886ca3b0580906d1c0ed98b674e0e6f15ecf721
Scott Houghton <scotthoughton@Scotts-MacBook-Pro.local>
2019-04-24 00:31:38 -0400

    Changed DNS NS from comma to new line list as required

--- modules/infra/dns.tf
+++ modules/infra/dns.tf
@@ -3,8 +3,8 @@ locals {
   resource_zone_id = "${element(concat(aws_route53_zone.pcf_zone.*.zone_id, list("")), 0)}"
   zone_id          = "${var.hosted_zone == "" ? local.resource_zone_id : local.data_zone_id}"
 
-  data_dns_name_servers     = "${join(",", flatten(concat(data.aws_route53_zone.pcf_zone.*.name_servers, list(list("")))))}"
-  resource_dns_name_servers = "${join(",", flatten(concat(aws_route53_zone.pcf_zone.*.name_servers, list(list("")))))}"
+  data_dns_name_servers     = "${join("\n", flatten(concat(data.aws_route53_zone.pcf_zone.*.name_servers, list(list("")))))}"
+  resource_dns_name_servers = "${join("\n", flatten(concat(aws_route53_zone.pcf_zone.*.name_servers, list(list("")))))}"
   name_servers              = "${var.hosted_zone == "" ? local.resource_dns_name_servers : local.data_dns_name_servers}"
   hosted_zone_count         = "${var.hosted_zone == "" ? 0 : 1}"
 }
