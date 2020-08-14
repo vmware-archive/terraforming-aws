df9f34e159186df82ea655166be5d6ec7a90ecdd
Wayne Adams <wadams@pivotal.io>
2020-08-14 15:21:15 -0700

    Allow mariadb version to float to latest
    - the previous mariadb version of 10.1 is no longer compatible
    with Credhub
    
    Co-authored-by: Wayne Adams <wadams@pivotal.io>
    Co-authored-by: Angela Chin <achin@pivotal.io>

--- terraforming-pas/main.tf
+++ terraforming-pas/main.tf
@@ -144,7 +144,6 @@ module "rds" {
   rds_instance_count = "${var.rds_instance_count}"
 
   engine         = "mariadb"
-  engine_version = "10.1.31"
   db_port        = 3306
 
   env_name           = "${var.env_name}"
